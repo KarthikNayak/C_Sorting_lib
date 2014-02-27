ee69eaba75011ee4e7377406a3c2160b69825fc0
Karthik Nayak <karthik.188@gmail.com>
2014-02-27 11:07:07 +0530

    Char tests for bubble sort

--- tests/bubblesort_tests.c
+++ tests/bubblesort_tests.c
@@ -2,6 +2,7 @@
 #include <bubble_sort.h>
 
 int a[] = {34, 23, 23, 2, 12, 35, 24, 54};
+char cha[8][20] = {"eclair", "kitkat", "icecreamsandwich", "froyo", "gingerbread", "honeycomb", "donut", "cupcake"};
 int size_a = 8;
 
 char *test_int(){
@@ -10,18 +11,29 @@ char *test_int(){
 	bubble_sort(a, size_a, sizeof(int), compare_int);
 	for(i = 1; i < size_a; i++)
 	{
-		if( a[i] < a[i-1])
-			return 1;
+		mu_assert(!(a[i] < a[i-1]), "Not sorted Int");			
 	}
 
 	return NULL;
 	
 }
 
+char *test_char(){
+	int i = 0;
+
+	bubble_sort(cha, size_a, 20, compare_string);
+	for(i = 1; i < size_a; i++){
+		mu_assert(!(strcmp(cha[i],cha[i-1]) < 0), "Not Sorted Char");			
+	}
+
+	return NULL;
+}
+
 char *all_tests() {
     mu_suite_start();
 
     mu_run_test(test_int);
+	mu_run_test(test_char);
 	return NULL;
 }
 
