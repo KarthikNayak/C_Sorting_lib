ab28b16439e0b3fc49d6129a6a8f193291d07341
Karthik Nayak <karthik.188@gmail.com>
2014-03-02 14:16:19 +0530

    Added Char * Comparator

--- src/bubble_sort.c
+++ src/bubble_sort.c
@@ -40,3 +40,10 @@ int compare_string(const void *a, const void *b)
 	const char *d = (char *)b;
 	return strcmp(c, d);
 }
+
+int compar_stringv2(const void *a, const void *b)
+{
+	const char **c = (char *)a;
+	const char **d = (char *)b;
+	return strcmp(c, d);
+}
--- tests/bubblesort_tests.c
+++ tests/bubblesort_tests.c
@@ -3,6 +3,7 @@
 
 int a[] = {34, 23, 23, 2, 12, 35, 24, 54};
 char cha[8][20] = {"eclair", "kitkat", "icecreamsandwich", "froyo", "gingerbread", "honeycomb", "donut", "cupcake"};
+char *cha2[] = {"eclair", "kitkat", "icecreamsandwich", "froyo", "gingerbread", "honeycomb", "donut", "cupcake"};
 int size_a = 8;
 
 char *test_int(){
@@ -29,11 +30,23 @@ char *test_char(){
 	return NULL;
 }
 
+char *test_char2(){
+	int i = 0;
+
+	bubble_sort(cha2, size_a, sizeof(char *), compare_string);
+	for(i = 1; i < size_a; i++){
+		mu_assert(!(strcmp(cha[i],cha[i-1]) < 0), "Not Sorted Char");			
+	}
+
+	return NULL;
+}
+
 char *all_tests() {
     mu_suite_start();
 
     mu_run_test(test_int);
 	mu_run_test(test_char);
+	mu_run_test(test_char2);
 	return NULL;
 }
 
