a98b5b082a1b52a7b8685f26e2c3a7c4918fa80c
Karthik Nayak <karthik.188@gmail.com>
2014-04-09 23:24:09 +0530

    Fixed the compare function for char ptr*[]
    Signed-off-by: Karthik Nayak <karthik.188@gmail.com>

--- src/compare.c
+++ src/compare.c
@@ -1,23 +1,23 @@
 #include <stdlib.h>
 #include <string.h>
 
-int compare_int(const void *a, const void *b)
+int compare_int(void *a, void *b)
 {
 	int *c = (int *)a;
 	int *d = (int *)b;
 	return *c - *d;
 }
 
-int compare_string(const void *a, const void *b)
+int compare_string(void *a, void *b)
 {
-	const char *c = (char *)a;
-	const char *d = (char *)b;
+	char *c = (char *)a;
+	char *d = (char *)b;
 	return strcmp(c, d);
 }
 
-int compare_stringv2(const void *a, const void *b)
+int compare_stringv2(void *a, void *b)
 {
-	const char **c = (char *)a;
-	const char **d = (char *)b;
-	return strcmp(c, d);
+	char **c = a;
+	char **d = b;
+	return strcmp(*c, *d);
 }
--- src/compare.h
+++ src/compare.h
@@ -1,8 +1,8 @@
 #ifndef compare_h
 #define compare_h
 
-int compare_int(const void *a, const void *b);
-int compare_string(const void *a, const void *b);
-int compare_stringv2(const void *a, const void *b);
+int compare_int(void *a, void *b);
+int compare_string(void *a, void *b);
+int compare_stringv2(void *a, void *b);
 
 #endif
